cf03d11fb7bf042b69fa0169658cf62b3ecc51de
Sohail khan <sohail0059@gmail.com>
2024-01-07 21:17:18 +0500

    Initial Commit

---
+++ RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by CRLHL-KHANNSOH2 on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
